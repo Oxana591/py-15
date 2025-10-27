82db66ed7ac66dea01f51872d08ad80ed678e602
Oxana591 <103951398+Oxana591@users.noreply.github.com>
2025-10-27 18:18:41 +0200

    1
    1

---
+++ fps_game.py
@@ -0,0 +1,59 @@
+import sys
+import pygame
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Циклічна анімація руху")
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+walk_images = [pygame.image.load(f"walk_{i}.png") for i in range(1,10)]
+jump_images=[pygame.image.load(f"jump_{i}.png") for i in range(1,6)]
+walk_index = 0
+jump_index = 0
+x, y = 100, 300
+is_jumping = False
+jump_height = 0
+max_jump_height = 50
+clock = pygame.time.Clock()
+player=pygame.Rect(x,y,20,20)
+fps = 15
+
+ANIMATION_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(ANIMATION_EVENT, 50)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        # elif event.type == ANIMATION_EVENT:
+        #     walk_index = (walk_index + 1) % len(walk_images)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not is_jumping:
+                is_jumping = True
+                jump_height = max_jump_height
+
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        walk_index = (walk_index + 1) % len(walk_images)
+        x+=1
+    elif keys[pygame.K_LEFT]:
+        x-=1
+
+    if is_jumping:
+        jump_index+=1
+        y -= 10
+        jump_height -= 10
+        if jump_height <= 0:
+            is_jumping = False
+            jump_index=0
+    else:
+        if y < 300:
+            y += 5
+
+    screen.fill((255, 255, 255))
+    screen.blit(walk_images[walk_index], (x, y))
+    screen.blit(jump_images[jump_index], (x, y))
+    pygame.display.flip()
+    clock.tick(fps)
