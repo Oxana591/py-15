c323c8c163413ec87e5a9f1c2a6c05856cd1bae2
Oxana591 <103951398+Oxana591@users.noreply.github.com>
2025-11-03 18:20:26 +0200

    mew img

--- fps_game.py
+++ fps_game.py
@@ -7,8 +7,9 @@ pygame.display.set_caption("Циклічна анімація руху")
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 
-walk_images = [pygame.image.load(f"walk_{i}.png") for i in range(1,10)]
-jump_images=[pygame.image.load(f"jump_{i}.png") for i in range(1,6)]
+walk_images = [pygame.image.load(f"images/walk_{i}.png") for i in range(1,10)]
+walk_left_images = [pygame.image.load(f"images/walk_left_{i}.png") for i in range(1,10)]
+jump_images=[pygame.image.load(f"images/jump_{i}.png") for i in range(1,6)]
 walk_index = 0
 jump_index = 0
 x, y = 100, 300
@@ -21,7 +22,7 @@ fps = 15
 
 ANIMATION_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(ANIMATION_EVENT, 50)
-
+left=False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,9 +37,12 @@ while True:
 
     keys=pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
+        left=False
         walk_index = (walk_index + 1) % len(walk_images)
         x+=1
     elif keys[pygame.K_LEFT]:
+        walk_index = (walk_index + 1) % len(walk_images)
+        left=True
         x-=1
 
     if is_jumping:
@@ -54,6 +58,9 @@ while True:
 
     screen.fill((255, 255, 255))
     screen.blit(walk_images[walk_index], (x, y))
-    screen.blit(jump_images[jump_index], (x, y))
+    if left:
+        screen.blit(walk_left_images[walk_index], (x, y))
+    if is_jumping:
+        screen.blit(jump_images[jump_index], (x, y))
     pygame.display.flip()
     clock.tick(fps)
